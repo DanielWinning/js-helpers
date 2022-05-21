b4bf27ccf2532dec52746fc74b04678c900a6f87
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-05-21 19:41:13 +0100

    added isObject and propertyIsDefined methods to Helper class, more tests. Need to add tests for propertyIsDefined and probably some more for the other methods

--- src/Helper.js
+++ src/Helper.js
@@ -3,8 +3,20 @@ class Helper {
         return variableToTest === null;
     }
 
-    static isDefined(variableToTest) {
-        return variableToTest !== undefined;
+    static isObject(variableToTest) {
+        return typeof variableToTest === "object" && !Helper.isNull(variableToTest) && !Array.isArray(variableToTest);
+    }
+
+    /**
+     *
+     * @param obj
+     * @param prop
+     */
+    static propertyIsDefined(obj, prop) {
+        if (Helper.isObject(obj)) {
+            return obj.prop !== undefined;
+        }
+        throw new Error("The given variable is not an object.");
     }
 }
 
--- tests/Helper.test.js
+++ tests/Helper.test.js
@@ -1,14 +1,34 @@
 const Helper = require("../src/Helper.js");
-const nullValue = null, string = "Hello, world!", number = 1;
+const nullValue = null, string = "Hello, world!", number = 1, obj = {}, arr = [];
 
-test("isNull($nullValue) equals true", () => {
+/**
+ * Helper.isNull()
+ */
+test("isNull(null) equals true", () => {
     expect(Helper.isNull(null)).toBe(true);
 });
-
-test("isNull($string) equals false", () => {
+test("isNull(string) equals false", () => {
     expect(Helper.isNull(string)).toBe(false);
 });
-
-test("isNull($number) equals false", () => {
+test("isNull(number) equals false", () => {
     expect(Helper.isNull(number)).toBe(false);
+});
+test("isNull(obj) equals false", () => {
+    expect(Helper.isNull(obj)).toBe(false);
+});
+
+/**
+ * Helper.isObject()
+ */
+test("isObject(null) equals false", () => {
+    expect(Helper.isObject(null)).toBe(false);
+});
+test("isObject(object) equals true", () => {
+    expect(Helper.isObject(obj)).toBe(true);
+});
+test("isObject(array) equals false", () => {
+    expect(Helper.isObject(arr)).toBe(false);
+});
+test("isObject(string) equals false", () => {
+    expect(Helper.isObject(string)).toBe(false);
 });
